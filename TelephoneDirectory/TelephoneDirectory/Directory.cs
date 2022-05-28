c8c0ae8560e157dfd94c16b518c42d79439feef0
Berk KARASU <98238241+Berk6@users.noreply.github.com>
2022-05-28 04:32:19 +0300

    Add files via upload

---
+++ TelephoneDirectory/TelephoneDirectory/Directory.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelephoneDirectory
+{
+    public class Directory
+    {
+        string name;
+        string surname;
+        long telNumber;
+        int selectedNumber;
+        int counter = 1;
+        List<string> Name = new List<string>();
+        List<string> Surname = new List<string>();
+        List<long> Number = new List<long>();
+
+        public void AddDirectory()
+        {
+            Console.Write(" Lütfen isim giriniz             :");
+            name = Console.ReadLine();
+            Console.Write(" Lütfen soyisim giriniz          :");
+            surname = Console.ReadLine();
+            Console.Write(" Lütfen telefon numarası giriniz :");
+            for (int i = 0; i < counter; i++)
+            {
+                try
+                {
+                    telNumber = long.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("girilen telefon numarası hatalıdır tekrar giriniz");
+                    counter++;
+                }
+            }
+            Name.Add(name);
+            Surname.Add(surname);
+            Number.Add(telNumber);
+        }
+        public void DeleteDirectory()
+        {
+            ListToDirectory();
+            Console.WriteLine("Hangi numara üzerinde işlem yapmak istiyorsunuz? Menuye donmek için 0'ı tuşlayın");
+            try
+            {
+                selectedNumber = int.Parse(Console.ReadLine()) - 1;
+            }
+            catch
+            {
+                Console.WriteLine("sayısal değer girin");
+                DeleteDirectory();
+                return;
+            }
+            if (selectedNumber == -1)
+            {
+                return;
+            }
+            if (selectedNumber > Name.Count - 1)
+            {
+                Console.WriteLine("Seçtiğiniz numaraya ait veri bulunamadı");
+                return;
+            }
+            Name.RemoveAt(selectedNumber);
+            Surname.RemoveAt(selectedNumber);
+            Number.RemoveAt(selectedNumber);
+        }
+        public void UpdateDirectory()
+        {
+            counter = 1;
+            ListToDirectory();
+            Console.WriteLine("Hangi numara üzerinde işlem yapmak istiyorsunuz? Menuye donmek için 0'ı tuşlayın");
+            try
+            {
+                selectedNumber = int.Parse(Console.ReadLine()) - 1;
+            }
+            catch
+            {
+                Console.WriteLine("sayısal değer girin");
+                UpdateDirectory();
+                return;
+            }
+            if (selectedNumber == -1)
+            {
+                return;
+            }
+            if (selectedNumber > Name.Count - 1)
+            {
+                Console.WriteLine("Seçtiğiniz numaraya ait veri bulunamadı");
+                return;
+            }
+            for (int i = 0; i < counter; i++)
+            {
+                Console.WriteLine("adı değiştirmek istiyorsanız(1)'e \n soyadı değiştirmek istiyorsanız(2)'e \n numarayı değiştirmek istiyorsanız(3)'e \n  basıp onaylayın ");
+                short number = short.Parse(Console.ReadLine());
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("yeni ad giriniz");
+                        Name[selectedNumber] = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("yeni soyad giriniz");
+                        Surname[selectedNumber] = Console.ReadLine();
+                        break;
+                    case 3:
+                        Console.WriteLine("yeni numara giriniz");
+                        Number[selectedNumber] = long.Parse(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("yanlış tuşlama yaptınız");
+                        counter++;
+                        break;
+                }
+            }
+        }
+        public void ListToDirectory()
+        {
+            for (int list = 0; list < Name.Count; list++)
+            {
+                Console.WriteLine($"{list + 1}. sıradaki kayıt");
+                Console.WriteLine(Name[list]);
+                Console.WriteLine(Surname[list]);
+                Console.WriteLine(Number[list]);
+            }
+        }
+        public void ListToDirectory(int indexNumber)
+        {
+            try
+            {
+                Console.WriteLine(Name[indexNumber]);
+                Console.WriteLine(Surname[indexNumber]);
+                Console.WriteLine(Number[indexNumber]);
+            }
+            catch
+            {
+                Console.WriteLine("aranılan ifade bulunamadı");
+            }
+        }
+    }
+}
---
+++ TelephoneDirectory/TelephoneDirectory/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TelephoneDirectory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool uygulama = true;
+                Directory directory = new Directory();
+            while (uygulama)
+            {
+                uint menuNumber = 0;
+                Console.WriteLine("Yapmak istediğiniz işlemi seçin");
+                Console.WriteLine("(1) Telefon Numarası Kaydetmek");
+                Console.WriteLine("(2) Telefon Numarası Silmek");
+                Console.WriteLine("(3) Telefon Numarası Güncellemek");
+                Console.WriteLine("(4) Rehber Listelemek");
+                Console.WriteLine("(5) Uygulamayı Sonlandırmak");
+                try
+                {
+                    menuNumber = uint.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("1-6 arasında sayısal değer giriniz");
+                    continue;
+                }
+                if (menuNumber > 6)
+                {
+                    Console.WriteLine("hatalı tuşlama yaptınız1");
+                    continue;
+                }
+                switch (menuNumber)
+                {
+                    case 1:
+                        directory.AddDirectory();
+                        break;
+                    case 2:
+                        directory.DeleteDirectory();
+                        break;
+                    case 3:
+                        directory.UpdateDirectory();
+                        break;
+                    case 4:
+                        directory.ListToDirectory();
+                        break;
+                    case 5:
+                        uygulama = false;
+                        break;
+                }
+            }
+        }
+    }
+}
